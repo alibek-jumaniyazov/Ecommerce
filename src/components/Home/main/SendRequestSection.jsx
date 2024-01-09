8e2f33ee796a894d5bca7ff5a37b1d0998268017
any amir <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-01-09 03:43:04 -1200

    add

--- src/components/Home/main/SendRequestSection.jsx
+++ src/components/Home/main/SendRequestSection.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 export default function SendRequestSection() {
+
+
   return (
     <div className='SendRequestSection'>
-
+      <div className="SendRequestSection__bgColor">
+        <div className="sendRequest__texts">
+          <p>An easy way to send requests to all suppliers</p>
+          <span>adipisicing</span>
+        </div>
+        <div className="sendRequest__post">
+          <p>Send quote to suppliers</p>
+          
+        </div>
+      </div>
     </div>
   )
 }
