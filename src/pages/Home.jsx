e94a06e4547e2764087e1754e3ba7567103c2126
any amir <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-01-09 01:17:47 -1200

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,63 @@ import Item from 'antd/es/list/Item'
 
 export default function Home() {
 
-  const [addProduct, setAddProduct] = useState([
-    bgImg1:{
+  const [addProduct, setAddProduct] = useState([{
+    bgImg1: {
       id: 1,
       img: productBGImg,
       title: "Home and outdoor"
-},
-    {
-
-    }
-  ])
+    },
+    productInfo: [
+      {
+        id: 1,
+        name: "Soft chairs",
+        from: 'From ',
+        price: 100
+      },
+      {
+        id: 2,
+        name: "Soft chairs",
+        from: 'From ',
+        price: 100
+      },
+      {
+        id: 3,
+        name: "Soft chairs",
+        from: 'From ',
+        price: 100
+      },
+      {
+        id: 4,
+        name: "Soft chairs",
+        from: 'From ',
+        price: 100
+      },
+      {
+        id: 5,
+        name: "Soft chairs",
+        from: 'From ',
+        price: 100
+      },
+      {
+        id: 6,
+        name: "Soft chairs",
+        from: 'From ',
+        price: 100
+      },
+      {
+        id: 7,
+        name: "Soft chairs",
+        from: 'From ',
+        price: 100
+      },
+      {
+        id: 8,
+        name: "Soft chairs",
+        from: 'From ',
+        price: 100
+      },
+    ]
+  }])
 
   return (
     <div className='Home'>
@@ -24,7 +71,7 @@ export default function Home() {
       <TimeSection />
       {
         addProduct.map((item) => (
-          <AddSection item={item}/>
+          <AddSection item={item} />
         ))
       }
     </div>
