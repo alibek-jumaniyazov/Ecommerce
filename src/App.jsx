598ea5aa9e9d5ab8ec801969298318ad11335bcf
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-01-20 00:58:14 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import Subscribe from "./components/Home/main/Subscribe"
+import CategoryUser from "./components/Home/main/CategoryUser"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="category/:name" element={<CategoryProduct />} />
         </Routes>
       </div>
       <Subscribe />
