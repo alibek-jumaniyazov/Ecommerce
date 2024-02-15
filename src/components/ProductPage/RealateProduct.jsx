cd3b7d696f9052301e0a4b1e2f50a9ee9a9e0498
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-02-15 14:42:34 +0500

    add

---
+++ src/components/ProductPage/RealateProduct.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function RealateProduct() {
+  return (
+    <div className='RealateProduct'>
+            <div className="realateProduct__img">
+                <img src="" alt="" />
+            </div>
+            <p className="realateProduct__title"></p>
+            <span className="realateProduct__price"></span>
+    </div>
+  )
+}
---
+++ src/components/ProductPage/Relate.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import img from '../../assets/icons/realateProduct.png'
+
+export default function Relate() {
+
+  const [product , setProduct] = useState([
+    {
+      id:1,
+      img:'',
+      tite:"Xiaomi Redmi 8 Original ",
+      price:"$32.00-$40.00"
+    },
+    {
+      id:2,
+      img:'',
+      tite:"Xiaomi Redmi 8 Original ",
+      price:"$32.00-$40.00"
+    },
+    
+  ])
+
+  return (
+    <div className='Relate'>
+      <h1 className="realteTitle">Related products</h1>
+
+    </div>
+  )
+}
+
+
+
+hgfjhsgfjshgdf  sdfaghsdgggdfs  sahjdgasd bjjfgsdf sdnbfhkjsgfjhkgsdufhgsdjhgfgfggggggfgfgfggfgfgb  s df nsdfgggggd sjgasdb ashd shgggdggggdggfffffs v    gggg  sgggg,kjhggfd      tuyt  njji76568yuthgj bnvmb nh
